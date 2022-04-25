3d639695a917fb38e5a130d4c94cbda346dc7f7d
tataki26 <tataki26@gmail.com>
2022-04-25 11:43:15 +0900

    Add webview command and html code for radio button

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,39 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	vscode.window.registerTreeDataProvider('emotionDevice', new TreeDataProvider());
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('uniDevice.start', () => {
+		  // Create and show panel
+		  const panel = vscode.window.createWebviewPanel(
+			'uniDevice',
+			'UNI Device',
+			vscode.ViewColumn.One,
+			{}
+		  );
+	
+		  // And set its HTML content
+		  panel.webview.html = getWebviewContent();
+		})
+	  );
+
 }
+
+function getWebviewContent() {
+	return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>UNI Device</title>
+  </head>
+  <body>
+	  <input type="radio" name="chk_info" value="USB">USB
+	  <input type="radio" name="chk_info" value="UDP" checked="checked">UDP
+	  <input type="radio" name="chk_info" value="UDP Full Address">UDP Full Address
+  </body>
+  </html>`;
+  }
 
 class TreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
 	onDidChangeTreeData?: vscode.Event<TreeItem|null|undefined>|undefined;
